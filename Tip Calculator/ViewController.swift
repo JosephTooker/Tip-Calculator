13ffaef7325270ff64881358d4b69af725b9d61f
Joseph Tooker <tookerjt@gmail.com>
2020-01-25 14:00:27 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Joseph Tooker on 1/25/20.
+//  Copyright © 2020 Joseph Tooker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
